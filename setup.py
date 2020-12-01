df4100cbf2ccf3b75b627f660589e838c78247f6
Ji Hyun Bak <jihyun.bak@gmail.com>
2020-12-01 14:49:08 -0800

    add setup and environment

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='nsds_lab_to_nwb',
+    version='0.0.1.dev0',
+    description=('Convert NSDS Lab data to NWB files.'),
+    author='Ji Hyun Bak',
+    author_email='jhbak@lbl.gov',
+    url='https://github.com/BouchardLab/nsds_lab_to_nwb',
+    packages=find_packages(),
+)
