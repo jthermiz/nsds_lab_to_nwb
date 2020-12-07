524f6023b5c59b72fee25f466a8504a702a6322c
John Hermiz <jthermiz@gmail.com>
2020-12-07 14:34:28 -0800

    Added tdt data and parameter extraction code. Need to discuss architecture mods to support iterative write from tdt->nwb

--- nsds_lab_to_nwb/components/tdt/tdt_manager.py
+++ nsds_lab_to_nwb/components/tdt/tdt_manager.py
@@ -1,3 +1,5 @@
+import tdt
+import datetime as dt
 from pynwb.ecephys import ElectricalSeries
 
 
@@ -14,13 +16,12 @@ class TdtManager():
         - dev_conf: (dict) metadata for the device.
                            nwb_builder.metadata['device'][device_name]
         - electrode_table_region: NWB electrode table region for the device
-
         Returns:
         - e_series: (ElectricalSeries) to be added to the NWB file
         '''
-        data = self.__extract_from_tdt_files(device_name, dev_conf)
-        # starting_time = 0.0                   # should confirm
-        # rate = dev_conf['sampling_rate']      # should confirm
+        data, tdt_params = self.__extract_from_tdt_files(device_name, dev_conf)
+        starting_time = tdt_params['start_time']
+        rate = tdt_params['sample_rate']
 
         # Create the electrical series
         e_series = ElectricalSeries(name=device_name,
@@ -34,5 +35,31 @@ class TdtManager():
 
     def __extract_from_tdt_files(self, device_name, dev_conf):
         tdt_path = self.tdt_path
-        # return data
-        raise NotImplementedError('TODO')
+        header = tdt.read_block(tdt_path, headers=1)
+        num_channels = len(header.stores[device_name].chan)
+        all_tdt_data = tdt.read_block(tdt_path) #I see no way to individually pull data streams using tdt's package, inefficent!
+        data = all_tdt_data.streams[device_name].data.T
+        # Iteratively write one channel at a time (TODO!)
+        # data = DataChunkIterator(data=iter_tdt_channels(tdt_path, device_name, header, num_channels),
+        #                          maxshape= (None, num_channels), 
+        #                          buffer_size=BUFFER_SIZE) 
+        tdt_params = {}
+        tdt_params['start_time'] = dt.datetime.fromtimestamp(header.start_time[0]) #i think this is right (confirm timestamp is accurate with another dataset)
+        tdt_params['stop_time'] = dt.datetime.fromtimestamp(header.stop_time[0])
+        tdt_params['sample_rate'] = header.stores[device_name].fs
+        return data, tdt_params
+    
+    def __iter_tdt_channels(self, device_name, header, num_channels):
+        #This method does not work, but could be a starting point for implementing iterative write to nwb
+        tdt_path = self.tdt_path
+        for ch in range(num_channels):
+            tdt_ch = ch + 1
+            tdt_struct = tdt.read_block(tdt_path, 
+                                        channel=tdt_ch, 
+                                        #headers=header, 
+                                        evtype=['streams'])
+            ch_data = tdt_struct.streams[device_name].data
+            print(ch_data.shape)
+            yield ch_data
+        return
+        
