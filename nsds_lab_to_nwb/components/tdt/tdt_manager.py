35df889baf5171ff096907e4bb8973404b2131d3
John Hermiz <jthermiz@gmail.com>
2020-12-08 21:13:53 -0800

    Got iterative write to work!

--- nsds_lab_to_nwb/components/tdt/tdt_manager.py
+++ nsds_lab_to_nwb/components/tdt/tdt_manager.py
@@ -1,7 +1,9 @@
 import tdt
-import datetime as dt
+import numpy as np
 from pynwb.ecephys import ElectricalSeries
+from hdmf.data_utils import DataChunkIterator
 
+BUFFER_SIZE = 32 #Buffer these many channels into a chunk
 
 class TdtManager():
     def __init__(self, tdt_path):
@@ -29,37 +31,41 @@ class TdtManager():
                                     electrodes=electrode_table_region,
                                     starting_time=starting_time,
                                     rate=rate,
-                                    **add_other_fields_as_necessary
+                                    # **add_other_fields_as_necessary
                                     )
         return e_series
-
-    def __extract_from_tdt_files(self, device_name, dev_conf):
-        tdt_path = self.tdt_path
-        header = tdt.read_block(tdt_path, headers=1)
-        num_channels = len(header.stores[device_name].chan)
-        all_tdt_data = tdt.read_block(tdt_path) #I see no way to individually pull data streams using tdt's package, inefficent!
-        data = all_tdt_data.streams[device_name].data.T
-        # Iteratively write one channel at a time (TODO!)
-        # data = DataChunkIterator(data=iter_tdt_channels(tdt_path, device_name, header, num_channels),
-        #                          maxshape= (None, num_channels), 
-        #                          buffer_size=BUFFER_SIZE) 
-        tdt_params = {}
-        tdt_params['start_time'] = dt.datetime.fromtimestamp(header.start_time[0]) #i think this is right (confirm timestamp is accurate with another dataset)
-        tdt_params['stop_time'] = dt.datetime.fromtimestamp(header.stop_time[0])
-        tdt_params['sample_rate'] = header.stores[device_name].fs
-        return data, tdt_params
     
-    def __iter_tdt_channels(self, device_name, header, num_channels):
-        #This method does not work, but could be a starting point for implementing iterative write to nwb
-        tdt_path = self.tdt_path
+    def __iter_tdt_channels(self, tdt_path, stream, header, num_channels):
+        #this is inefficient because it loads all streams. As far as I can tell,
+        #there's no way to select streams to load
         for ch in range(num_channels):
             tdt_ch = ch + 1
             tdt_struct = tdt.read_block(tdt_path, 
                                         channel=tdt_ch, 
-                                        #headers=header, 
+                                        headers=header, 
                                         evtype=['streams'])
-            ch_data = tdt_struct.streams[device_name].data
-            print(ch_data.shape)
+            ch_data = tdt_struct.streams[stream].data
+            ch_data = np.reshape(1, -1) #weird quirk to make data samples x channels since it's tranposed downstream
             yield ch_data
         return
+
+    def __extract_from_tdt_files(self, device_name, dev_conf):
+        tdt_path = self.tdt_path
+        header = tdt.read_block(tdt_path, headers=1)
+        tdt_tmp = tdt.read_block(tdt_path, channel=1)
+        num_channels = len(header.stores[device_name].chan)
+        num_samples = len(tdt_tmp.streams[device_name].data)
+        # Iteratively write one channel at a time 
+        data = DataChunkIterator(data=self.__iter_tdt_channels(tdt_path, device_name, header, num_channels),
+                                  maxshape= (num_samples, num_channels), 
+                                  buffer_size=BUFFER_SIZE) 
+        tdt_params = {}
+        tdt_params['start_time'] = header.start_time[0] #returning numeric since that's what nwb wants for an eseries
+        tdt_params['stop_time']  = header.stop_time[0]
+        #tdt_params['stop_time'] = dt.datetime.fromtimestamp(header.stop_time[0])
+        tdt_params['sample_rate'] = header.stores[device_name].fs
+        return data, tdt_params   
+
+    
+
         
