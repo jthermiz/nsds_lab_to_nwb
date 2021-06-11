66e52fa20460176cdc902f4986ca7db90f3aa47e
Ji Hyun Bak <jihyun.bak@gmail.com>
2021-06-10 20:50:14 -0700

    fix metadata path in example script

--- scripts/RVG02_B09_test.sh
+++ scripts/RVG02_B09_test.sh
@@ -2,6 +2,6 @@
 
 SAVE_PATH="_test/"
 BLOCK_NAME="RVG02_B09"
-BLOCK_META="../tests/_data/RVG02/block_data.csv"
+BLOCK_META="${NSDS_DATA_PATH}/RVG02/block_data.csv"
 
 python generate_nwb.py  $SAVE_PATH $BLOCK_NAME $BLOCK_META
