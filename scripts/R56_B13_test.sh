258d463fd07255ecafd927039f8420260b23cb24
Ji Hyun Bak <jihyun.bak@gmail.com>
2021-06-10 17:59:10 -0700

    clear path variables from scripts

--- scripts/R56_B13_test.sh
+++ scripts/R56_B13_test.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# set base paths: customize as needed,
-# or comment out if you already set these up as global environment variables
-export NSDS_METADATA_PATH="${HOME}/Src/NSDSLab-NWB-metadata/"
-export NSDS_DATA_PATH="/clusterfs/NSDS_data/hackathon20201201/TTankBackup/"
-export NSDS_STIMULI_PATH="/clusterfs/NSDS_data/hackathon20201201/Stimuli/"
-
 SAVE_PATH="_test/"
 BLOCK_NAME="R56_B13"
 BLOCK_META="${NSDS_METADATA_PATH}/auditory/yaml/block/R56/R56_B13.yaml"
