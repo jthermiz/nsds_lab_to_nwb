ab26735f54f569068d33dcae3b577391a7d67051
Ji Hyun Bak <jihyun.bak@gmail.com>
2021-06-21 17:07:45 -0700

    add test for ExpNoteReader

---
+++ tests/test_exp_note_reader.py
@@ -0,0 +1,24 @@
+import os
+import unittest
+
+from nsds_lab_to_nwb.metadata.exp_note_reader import ExpNoteReader
+from nsds_lab_to_nwb.utils import get_data_path, split_block_folder
+
+
+class TestCase_ExpNoteReader(unittest.TestCase):
+
+    data_path = get_data_path()
+    write_path = '_test/'
+
+    def test_ods_to_yaml(self):
+        ''' read an RFLYY_Experiment_Notes.ods file,
+        and write a RFLYY_BXX.yaml file.
+        '''
+        block_folder = 'RVG16_B02'
+        _, animal_name, _ = split_block_folder(block_folder)
+        path = os.path.join(self.data_path, animal_name, block_folder)
+        reader = ExpNoteReader(path, block_folder)
+        reader.dump_yaml(write_path=self.write_path)
+
+if __name__ == '__main__':
+    unittest.main()
