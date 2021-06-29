f2a4ce7ba04dbda8f89013f2630cb1f4b8793071
Ji Hyun Bak <jihyun.bak@gmail.com>
2021-06-28 17:13:47 -0700

    add description to electrode table region, and minor cleanup

--- nsds_lab_to_nwb/components/electrode/electrodes_originator.py
+++ nsds_lab_to_nwb/components/electrode/electrodes_originator.py
@@ -1,5 +1,5 @@
-import numpy as np
 import itertools
+
 
 class ElectrodesOriginator():
     def __init__(self, metadata):
@@ -12,13 +12,13 @@ class ElectrodesOriginator():
 
     def __add_electrodes(self, nwb_content):
         device_electrode_regions = {}
-        e_id_gen = itertools.count() #Electrode ID, unique for channels across devices
+        e_id_gen = itertools.count()    # Electrode ID, unique for channels across devices
         for device_name in nwb_content.devices:
-            device = nwb_content.devices[device_name]
+            # device = nwb_content.devices[device_name]
             e_group = nwb_content.electrode_groups[device_name]
             dev_config = self.metadata['device'][device_name]
 
-            ## Add each electrode
+            # Add each electrode
             electrode_region = []
             ch_ids = dev_config['ch_ids']
             ch_pos = dev_config['ch_pos']
@@ -41,9 +41,9 @@ class ElectrodesOriginator():
     def __create_electrode_table_regions(self, nwb_content, device_electrode_regions):
         e_regions = {}
         for device_name, electrode_region in device_electrode_regions.items():
-            ## Create the electrode table region for this device
+            # Create the electrode table region for this device
             table_region = nwb_content.create_electrode_table_region(
                                 region=electrode_region,
-                                description='')
+                                description=f'Electrode table region for device {device_name}')
             e_regions[device_name]=table_region
         return e_regions
